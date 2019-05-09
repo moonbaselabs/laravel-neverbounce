960a0cf248dc17717ca76f0d094c1c2c958e805e
Nic Wentling <nic@moonbaselabs.co>
2019-05-09 12:24:08 -0500

    Create service provider

---
+++ src/NeverBounceWrapperServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MoonbaseLabs\NeverBounceWrapper;
+
+use Illuminate\Support\ServiceProvider;
+
+class NeverBounceWrapperServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
