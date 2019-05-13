462aea1faea825218619c22b36959d4b2c61a522
Nic Wentling <nic@moonbaselabs.co>
2019-05-13 11:33:10 -0500

    Add functionality to boot and register methods.

--- src/NeverBounceServiceProvider.php
+++ src/NeverBounceServiceProvider.php
@@ -2,10 +2,15 @@
 
 namespace MoonbaseLabs\NeverBounce;
 
+use NeverBounce\Auth;
+use NeverBounce\Single;
 use Illuminate\Support\ServiceProvider;
 
 class NeverBounceServiceProvider extends ServiceProvider
 {
+    protected $suggestion;
+    protected $error;
+
     /**
      * Bootstrap services.
      *
@@ -13,6 +18,36 @@ class NeverBounceServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $validator = $this->app->make(Validator::class);
+
+        $this->app->validator->extend('neverbounce', function ($attribute, $value) use ($validator) {
+            return $validator->validate($value);
+        });
+
+        $this->app->validator->replacer('neverbounce', function ($message) use ($validator) {
+            $suggestion = $validator::$correction;
+            $error = $validator::$error;
+
+            if (!$suggestion) {
+                return str_replace([':suggestion', ':error'], '', $message);
+            }
+
+            if ($suggestion) {
+                return str_replace(
+                    [':suggestion', ':error'],
+                    ["Did you mean {$suggestion}?", ''],
+                    $message
+                );
+            }
+
+            if ($error) {
+                return str_replace(
+                    [':error', ':suggestion', 'The email is not valid. '],
+                    ["There was an error. {$error}", '', ''],
+                    $message
+                );
+            }
+        });
     }
 
     /**
@@ -22,6 +57,12 @@ class NeverBounceServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $config = $this->app->config['services']['neverbounce'];
+
+        Auth::setApiKey($config['api_key']);
+
+        $this->app->singleton(Validator::class, function () {
+            return new Validator(new Single);
+        });
     }
 }
